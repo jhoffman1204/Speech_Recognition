9e302af000dfde5e0d16c831d4dd83ae7f4dccd7
jaxmith <jack.smith@stonybrook.edu>
2018-04-17 18:00:55 -0400

    removed unnecessary plotting/printing

--- speakeridentification.py
+++ speakeridentification.py
@@ -1,23 +1,12 @@
 import numpy as np
 import librosa
 import librosa.display
-import matplotlib.pyplot as plt
 import scipy.signal
 
 audio = '.\\Desktop\\speech_test\\clean\\s2_bbbs5p.wav'
 data, sampling_rate = librosa.load(audio, sr=4000)
-print(data.size)
-plt.figure(figsize=(12, 4))
-plt.plot(data)
-plt.show()
-print("y:", data, end='\n\n')
 spectral = 10 * np.log10(np.abs(librosa.core.stft(data, n_fft=512, hop_length=80, window=scipy.signal.hanning)))
-print(spectral.shape)
-plt.plot(spectral[:,50])
-plt.show()
-print("Spectral Vector:")
-print(spectral)
+w, h = spectral.shape
+spectral = spectral.reshape(w * h)
 
-
-# Notes: 
-# kevinsprojects.wordpress.com/2014/12/13
+#create loop to concatenate all the spectrals for designated speaker
